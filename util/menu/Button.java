052609622b92a8c456010ecd3fba250883811902
TMUKingston <10081997@gmx.de>
2015-12-11 23:00:29 +0100

    TextBox animaton

--- util/menu/Button.java
+++ util/menu/Button.java
@@ -1,33 +1,34 @@
 package util.menu;
 
-import util.controls.Mouse;
-import util.geometry.Bounding;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
+import util.controls.Mouse;
+import util.geometry.Bounding;
 
 public abstract class Button {
-	protected String title;
-	protected Bounding b;
-	protected boolean selected;
 
-	public Button(String title, Bounding b) {
-		this.b = b;
-		this.title = title;
-		selected = false;
-	}
+    protected String title;
+    protected Bounding b;
+    protected boolean selected;
 
-	public boolean update() {
-		if (b.intersects(Mouse.getPos())) {
-			selected = true;
-			if (Mouse.isKeyDown(MouseEvent.BUTTON1)) {
-				System.out.println(true);
-				return true;
-			}
-		} else {
-			selected = false;
-		}
-		return false;
-	}
+    public Button(String title, Bounding b) {
+        this.b = b;
+        this.title = title;
+        selected = false;
+    }
 
-	public abstract void render(Graphics2D g);
+    public boolean update() {
+        if (b.intersects(Mouse.getPos())) {
+            selected = true;
+            if (Mouse.isKeyDown(MouseEvent.BUTTON1)) {
+                System.out.println(true);
+                return true;
+            }
+        } else {
+            selected = false;
+        }
+        return false;
+    }
+
+    public abstract void render(Graphics2D g);
 }
