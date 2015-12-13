d16a84bbf367b3fddaf296ec150252741125dbb3
Markus-Brand <markusbrand97@gmail.com>
2015-12-13 23:04:08 +0100

    Restart text

--- logic/GameOverScreen.java
+++ logic/GameOverScreen.java
@@ -6,7 +6,10 @@
 package logic;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import main.Options;
+import util.gfx.TextBoxView;
 
 /**
  *
@@ -14,9 +17,36 @@ import java.awt.Graphics2D;
  */
 public class GameOverScreen {
 
-    void draw(Graphics2D g) {
-        g.setColor(new Color(0, 0, 0, 220));
-        g.fillRect(200, 200, 1200, 500);
+    float timeVisible = 0;
+
+    void update(float timeSinceLastFrame) {
+        timeVisible += timeSinceLastFrame;
     }
-    
+
+    void draw(Graphics2D g) {
+        if (timeVisible < 2) {
+            System.out.println(timeVisible * 110f);
+            g.setColor(new Color(50, 50, 50, (int) (timeVisible * 110f)));
+            g.fillRect(200, 200, 1200, 500);
+        } else {
+            g.setColor(new Color(50, 50, 50, 220));
+            g.fillRect(200, 200, 1200, 500);
+            
+            g.setColor(Color.LIGHT_GRAY);
+            g.setFont(new Font("Helvetica", Font.BOLD, 100));
+            
+            int gowidth = (int)TextBoxView.getSize(g, "Game Over").getWidth();
+            int scorewidth = (int)TextBoxView.getSize(g, "Score: "+ (int) Options.score).getWidth();
+            
+            g.drawString("Game Over", 200 + (1200 - gowidth) / 2, 330);
+            g.drawString("Score:" + (int) Options.score, 200 + (1200 - scorewidth) / 2, 450);
+            
+            g.setFont(new Font("Helvetica", Font.BOLD, 50));
+            
+            int newwidth = (int)TextBoxView.getSize(g, "Press ENTER to restart").getWidth();            
+            
+            g.drawString("Press ENTER to restart", 200 + (1200 - newwidth) / 2, 620);
+        }
+    }
+
 }
