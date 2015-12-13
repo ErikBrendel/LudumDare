7970216cbac30274bf69594d23622ced514d194e
Markus-Brand <markusbrand97@gmail.com>
2015-12-13 14:01:05 +0100

    Laser

--- logic/Pickup.java
+++ logic/Pickup.java
@@ -19,7 +19,7 @@ import util.gfx.GfxLoader;
  */
 public abstract class Pickup extends FlyingObject {
 
-    public static final int PickupCount = 1;
+    public static final int PickupCount = 2;
 
     private final int id;
 
@@ -35,26 +35,24 @@ public abstract class Pickup extends FlyingObject {
 
         if (height == 0) {
             render = images[0][id];
+        } else if (height == 1) {
+            render = images[1][id];
+        } else if (height == -1) {
+            render = images[1][id];
         } else {
-            if (height == 1) {
-                render = images[1][id];
-            } else if (height == -1) {
-                render = images[1][id];
-            } else {
-                float factor = Math.abs(height);
-                //System.err.println("factor = " + factor);
-                BufferedImage render1 = images[0][id]; //factor == 0
-                BufferedImage render2 = images[1][id]; //factor == 1
-                //render = GfxLoader.combine(render1, render2, factor);
-                g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1 - factor));
-                g.drawImage(render1, dX, dY, null);
-                g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, factor));
-                g.drawImage(render2, dX, dY, null);
-                g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+            float factor = Math.abs(height);
+            //System.err.println("factor = " + factor);
+            BufferedImage render1 = images[0][id]; //factor == 0
+            BufferedImage render2 = images[1][id]; //factor == 1
+            //render = GfxLoader.combine(render1, render2, factor);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1 - factor));
+            g.drawImage(render1, dX, dY, null);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, factor));
+            g.drawImage(render2, dX, dY, null);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
 
-                render = render1;
-                return;
-            }
+            render = render1;
+            return;
         }
         g.drawImage(render, dX, dY, null);
         //g.setColor(Color.red);
@@ -70,7 +68,7 @@ public abstract class Pickup extends FlyingObject {
         b.setY(location.getY());
         return 0;
     }
-    
+
     public abstract void doEffect(Player p);
 
     //
@@ -92,13 +90,29 @@ public abstract class Pickup extends FlyingObject {
             public void doEffect(Player p) {
                 p.damage(p.getHealth() - 100);
             }
-            
+
         };
+
         Random r = new Random();
         p.location = new Point(1700, -200 + r.nextInt(1100));
         p.moveVector = new Point(-150 + r.nextInt(100), 40 - r.nextInt(80));
         p.rotateSpeed = -100 + r.nextInt(200);
         return p;
+    }
+
+    public static Pickup createLaser() {
+        Pickup laser = new Pickup(1) {
+            @Override
+            public void doEffect(Player p) {
+                System.out.println("LASERS!");
+            }
+        };
+        
+        Random r = new Random();
+        laser.location = new Point(1700, -200 + r.nextInt(1100));
+        laser.moveVector = new Point(-150 + r.nextInt(100), 40 - r.nextInt(80));
+        laser.rotateSpeed = -100 + r.nextInt(200);
+        return laser;
     }
 
 }
