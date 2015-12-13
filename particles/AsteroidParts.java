f0bdec0634b939fa0d0764f5e70a480b4198759d
TMUKingston <10081997@gmx.de>
2015-12-13 16:34:00 +0100

    asteroidParts - particle

---
+++ particles/AsteroidParts.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package particles;
+
+import java.awt.Color;
+import static particles.Particle.ran;
+import util.geometry.Point;
+
+/**
+ *
+ * @author Erik
+ */
+public class AsteroidParts extends Particle {
+
+    private static Color[] colors;
+
+    static {
+        colors = new Color[40];
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = new Color(2 * i + 50, 2 * i + 50, 2 * i + 50, 100);
+        }
+    }
+
+    @Override
+    public Particle clone() {
+        return new AsteroidParts((int) (x), (int) (y));
+    }
+
+    public AsteroidParts(int x, int y) {
+        super(x - 50 + ran.nextInt(101), y - 50 + ran.nextInt(101), colors[ran.nextInt(40)], ran.nextInt(20) + 10, ran.nextFloat() * 0.2f + 0.1f, new ParticleMover() {
+
+            float dir = ran.nextFloat() * 360;
+            Point moveVector = new Point(30 + ran.nextFloat() * 40, 0).rotate(dir);
+
+            @Override
+            public Point move(float x, float y, float deltaTime) {
+                Point p = new Point(x, y);
+
+                Point v = moveVector.trim(deltaTime * 100);
+                p = p.plus(v);
+
+                return p;
+            }
+        });
+    }
+}
